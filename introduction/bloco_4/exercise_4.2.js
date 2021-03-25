d33332a36d5f89533b5bb107c1fd5a3c9f80d3e1
heyynat <natalifplima@gmail.com>
2021-03-25 15:01:56 -0400

    Exercício 5 - Descobrir o maior elemento do array e imprimi-lo

--- introduction/bloco_4/exercise_4.2.js
+++ introduction/bloco_4/exercise_4.2.js
@@ -1,16 +1,12 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let resultado = 0;
-let media = 0;
+let maior = 0;
 
 for (let i = 0; i<numbers.length; i++){
-  resultado += numbers[i];
+    if (numbers[i]>maior){
+        maior = numbers[i];
+    }
 }
 
-media = resultado/numbers.length; 
+console.log(maior);
 
-if (media>20) {
-    console.log("Maior que 20");
-}
-else{
-    console.log("Menor que 20");
-}
+
