5beeb4b962620e32c13ef147f946b3fae620f104
heyynat <natalifplima@gmail.com>
2021-03-25 14:37:00 -0400

    Exercício 2 - Somar todos os valores contidos no array e imprimir o resultado

--- introduction/bloco_4/exercise_4.2.js
+++ introduction/bloco_4/exercise_4.2.js
@@ -1,2 +1,8 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-console.log(numbers);
+let resultado = 0;
+
+for (let i = 0; i<numbers.length; i++){
+  resultado += numbers[i];
+}
+
+console.log(resultado);
