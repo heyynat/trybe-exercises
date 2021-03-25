2361ff602f7642a070ccaf21771c5dfe46f14933
heyynat <natalifplima@gmail.com>
2021-03-25 15:12:01 -0400

    Exercício 6 - Descobrir quantos valores impares existem no array.

--- introduction/bloco_4/exercise_4.2.js
+++ introduction/bloco_4/exercise_4.2.js
@@ -1,12 +1,16 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let maior = 0;
+let impar = 0;
 
 for (let i = 0; i<numbers.length; i++){
-    if (numbers[i]>maior){
-        maior = numbers[i];
+    if (numbers[i] % 2 != 0){
+        impar++;
+    }
+    if (impar === 0) {
+        console.log("Nenhum valor ímpar encontrado.")
     }
 }
 
-console.log(maior);
+console.log("Existem", impar, "elementos impares.");
+
 
 
