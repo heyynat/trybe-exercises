37cdd5bc7417af786a3c53567d7085066ad10225
heyynat <natalifplima@gmail.com>
2021-03-29 16:12:11 -0400

    JavaScript - Primeiros passos - exercicio - 2

--- introduction/bloco_4/dia_1/exercise_4.1.js
+++ introduction/bloco_4/dia_1/exercise_4.1.js
@@ -1,10 +1,9 @@
 let a = 5;
 let b = 9;
 
-console.log('Adição entre a e b é', a+b);
-
-console.log('Subtração entre a e b é', a-b);
-
-console.log('Multiplicação entre a e b é', a*b);
-
-console.log('O módulo entre a e b é', a%b);
+if (a>b) {
+  console.log(a);
+}
+else{
+  console.log(b);
+}
