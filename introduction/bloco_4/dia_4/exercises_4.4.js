06fc4a984b6cdfec833c16553f3245d13365ca96
heyynat <natalifplima@gmail.com>
2021-04-05 14:11:10 -0400

    JavaScript - Objetos e funçoes - Parte I - exercise - 5

--- introduction/bloco_4/dia_4/exercises_4.4.js
+++ introduction/bloco_4/dia_4/exercises_4.4.js
@@ -5,6 +5,20 @@ let info = {
   recorrente: true
 };
 
+let info2 =
+{
+  personagem: 'Tio Patinhas',
+  origem: 'Pato Christmas on Bear Mountain, Dell´s Four Color Comics #178',
+  nota: 'O último MacPatinhas',
+  recorrente: true
+};
+
 for(let data in info) {
-  console.log(info[data]);
+  if (
+  info[data] === info2[data] &&
+  info.recorrente === info2.recorrente) {
+    console.log('Ambos recorrentes');
+} else {
+    console.log (info[data] + ' e ' + info2[data]);
+}
 }
