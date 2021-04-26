dfa61212d4667e021d7c3face3c01a2452b06a9e
heyynat <natalifplima@gmail.com>
2021-04-26 17:35:03 -0400

    Parte I - 2/2

--- introduction/bloco_7/dia_1/script.js
+++ introduction/bloco_7/dia_1/script.js
@@ -1,3 +1,4 @@
+// Parte I - 1/2
 const testingScope = escopo => { 
     if (escopo === true) { 
         let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
@@ -10,3 +11,27 @@ const testingScope = escopo => {
 }
 
 testingScope(true);
+
+// Parte I - 2/2
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+// Seu código aqui.
+const sortOddsAndEvens = () => {
+    oddsAndEvens[0] = 2;
+    oddsAndEvens[1] = 3;
+    oddsAndEvens[2] = 4;
+    oddsAndEvens[3] = 7;
+    oddsAndEvens[4] = 10;
+    oddsAndEvens[5] = 13;
+    
+    return oddsAndEvens;
+}
+
+const sortedArray = sortOddsAndEvens();
+console.log(`Os números ${sortedArray} se encontram ordenados de forma crescente !`);
+
+/* Bônus array.sort()
+* const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+*
+* console.log(`Os números ${oddsAndEvens.sort((a, b) => a - b)} se encontram ordenados de forma crescente !`);
+*/
