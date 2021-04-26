4e415a9bcf2bc2bf8d7416f055390aefc0455925
heyynat <natalifplima@gmail.com>
2021-04-26 17:49:24 -0400

    Parte II - 2/4

--- introduction/bloco_7/dia_1/script.js
+++ introduction/bloco_7/dia_1/script.js
@@ -46,3 +46,21 @@ const factorial = number => {
 }
 
 console.log(factorial(4));
+
+// Part II - 2/4
+const longestWord = text => {
+  let wordArray = text.split(' ')
+  let maxLength = 0
+  let result = ''
+
+  for (const word of wordArray) {
+    if (word.length > maxLength) {
+      maxLength = word.length;
+      result = word;
+    }
+  }
+  
+  return result;
+}
+
+console.log(longestWord("Antonio foi no banheiro e não sabemos o que aconteceu"));
