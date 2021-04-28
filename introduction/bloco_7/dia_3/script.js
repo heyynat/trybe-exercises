491017acdc83324dce618721403ff6bcb4e363c9
heyynat <natalifplima@gmail.com>
2021-04-28 18:23:30 -0400

    Exercise 1

---
+++ introduction/bloco_7/dia_3/script.js
@@ -0,0 +1,22 @@
+const assert = require('assert');
+
+function sum(a, b) {
+  if (typeof a !== 'number' || typeof b !== 'number') {
+    throw new Error('parameters must be numbers');
+  }
+  
+  return a + b;
+}
+
+// implemente seus testes aqui
+assert.strictEqual(sum(4, 5), 9, 'Incorreto! :/');
+assert.strictEqual(sum(0, 0), 0, 'Incorreto! :/');
+assert.strictEqual(sum(4, 5), 9, 'Incorreto! :/');
+assert.throws(() => { sum(4, "5"); });
+
+assert.throws(() => {
+  sum(4, '5');
+});
+assert.throws(() => {
+  sum(4, '5');
+}, /^Error: parameters must be numbers$/);
