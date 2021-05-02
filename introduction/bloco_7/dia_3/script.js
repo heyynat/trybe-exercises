6016f37eaf9602910bfccdfc4749f86efb7c2e7c
heyynat <natalifplima@gmail.com>
2021-05-02 09:25:25 -0400

    Exercicio 4

--- introduction/bloco_7/dia_3/script.js
+++ introduction/bloco_7/dia_3/script.js
@@ -61,3 +61,19 @@ const myArray2 = [1, 2, 3, 4];
 myRemoveWithoutCopy(myArray2, 1);
 assert.deepStrictEqual(myArray2.length, 3);
 assert.deepStrictEqual(myRemoveWithoutCopy([1, 2, 3, 4], 5), [1, 2, 3, 4], 'Esperava-se um array diferente');
+
+// Exercise 4
+function myFizzBuzz(num) {
+  if (typeof num !== 'number') return false;
+  if (num % 3 === 0 && num % 5 === 0) return 'fizzbuzz';
+  if (num % 3 === 0) return 'fizz';
+  if (num % 5 === 0) return 'buzz';
+  return num;
+}
+
+// implemente seus testes aqui
+assert.strictEqual(myFizzBuzz(15), 'fizzbuzz');
+assert.strictEqual(myFizzBuzz(9), 'fizz');
+assert.strictEqual(myFizzBuzz(25), 'buzz');
+assert.strictEqual(myFizzBuzz(17), 17);
+assert.strictEqual(myFizzBuzz('fizzbuzz'), false);
