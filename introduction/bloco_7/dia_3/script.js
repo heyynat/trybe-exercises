9d22d4c61803311c776ff7f03e6546e286ef3ba6
heyynat <natalifplima@gmail.com>
2021-05-02 09:11:15 -0400

    Exercicio 3

--- introduction/bloco_7/dia_3/script.js
+++ introduction/bloco_7/dia_3/script.js
@@ -41,3 +41,23 @@ const myArray = [5, 6, 7, 8];
 myRemove(myArray, 5);
 assert.deepStrictEqual(myArray, [5, 6, 7, 8]);
 assert.deepStrictEqual(myRemove([1, 2, 3, 4], 5), [1, 2, 3, 4]);
+
+// Exercise 3
+function myRemoveWithoutCopy(arr, item) {
+  for (let index = 0, len = arr.length; index < len; index += 1) {
+    if (arr[index] === item) {
+      arr.splice(index, 1);
+      index -= 1;
+      len -= 1;
+    }
+  }
+  return arr;
+}
+
+// implemente seus testes aqui
+assert.deepStrictEqual(myRemoveWithoutCopy([1, 2, 3, 4], 3), [1, 2, 4], 'Esperava-se um array diferente');
+assert.notDeepStrictEqual(myRemoveWithoutCopy([1, 2, 3, 4], 3), [1, 2, 3, 4]);
+const myArray2 = [1, 2, 3, 4];
+myRemoveWithoutCopy(myArray2, 1);
+assert.deepStrictEqual(myArray2.length, 3);
+assert.deepStrictEqual(myRemoveWithoutCopy([1, 2, 3, 4], 5), [1, 2, 3, 4], 'Esperava-se um array diferente');
