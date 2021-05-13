ced0c6225484477b502c561b3ffd5876cc65ef4a
heyynat <natalifplima@gmail.com>
2021-05-13 11:38:19 -0400

    JavaScript Assíncrono e Promises - Exercicio 2

--- introduction/bloco_9/dia_2/script.js
+++ introduction/bloco_9/dia_2/script.js
@@ -2,15 +2,35 @@
 const API_URL = 'https://icanhazdadjoke.com/';
 
 const fetchJoke = () => {
-    const myObject = {
-        method: 'GET',
-        headers: { 'Accept': 'application/json' }
-    };
-    
-    fetch(API_URL, myObject)
-    .then(response => response.json())
-    .then(data =>
-        document.getElementById('jokeContainer').innerText = data.joke
-      );
+const myObject = {
+ method: 'GET',
+ headers: { 'Accept': 'application/json' }
 };
-window.onload = () => fetchJoke();
+
+fetch(API_URL, myObject)
+.then(response => response.json())
+.then(data =>
+ document.getElementById('jokeContainer').innerText = data.joke
+ );
+};
+// window.onload = () => fetchJoke();
+
+// Exercicio 2
+const fetchPromise = () => {
+const myPromise = new Promise ((resolve, reject) => {
+ const myArray = Array.from(
+  { length: 10 },
+  () =>  Math.floor(Math.random() * 50) + 1
+  );
+  const sum = myArray.map(number => number * number)
+  .reduce((number, acc) => number + acc, 0);
+  
+  (sum < 8000) ? resolve(sum) : reject();
+ });
+ 
+ myPromise
+ .then(() => console.log('Promise resolvida'))
+ .catch(() => console.log('Promise rejeitada'));
+};
+
+fetchPromise();
