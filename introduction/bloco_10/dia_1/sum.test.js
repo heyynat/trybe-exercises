40fd8269e4e592d1665d316a24fa3c88904b3b5b
heyynat <natalifplima@gmail.com>
2021-05-18 17:56:14 -0400

    Parte I

---
+++ introduction/bloco_10/dia_1/sum.test.js
@@ -0,0 +1,23 @@
+function sum(a, b) {
+  if (typeof a !== 'number' || typeof b !== 'number') {
+    throw new Error('parameters must be numbers');
+  }
+
+  return a + b;
+}
+
+test('Teste se o retorno de sum(4, 5) é 9', () => {
+  expect(sum(4, 5)).toEqual(9);
+});
+
+test('Teste se o retorno de sum(0, 0) é 0', () => {
+  expect(sum(0, 0)).toEqual(0);
+});
+
+test('Teste se a função sum lança um erro quando os parâmetros são 4 e "5" (string 5)', () => {
+  expect(() => sum(4, "5")).toThrow();
+});
+
+test('Teste se a mensagem de erro é "parameters must be numbers" quando realizar a chamada sum(4, "5")', () => {
+  expect(() => sum(4, "5")).toThrowError(new Error("parameters must be numbers"));
+});
