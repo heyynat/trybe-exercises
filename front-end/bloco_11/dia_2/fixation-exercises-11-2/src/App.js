8fee0de0ad03b0da5e325e85ce5a91523d94ddc9
heyynat <natalifplima@gmail.com>
2021-05-25 20:41:56 -0400

    Exercicios de fixaçao - Componentes React 3

--- front-end/bloco_11/dia_2/fixation-exercises-11-2/src/App.js
+++ front-end/bloco_11/dia_2/fixation-exercises-11-2/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Image from './Image';
 import Order from './Order';
+import UserProfile from './UserProfile';
 
 class App extends React.Component {
   render() {
@@ -28,6 +29,21 @@ class App extends React.Component {
     const shoppingList = ['leite', 'arroz', 'feijão', 'banana', 'carne'];
     const items = shoppingList.map((item, index) => (<li key={ index }>{ item }</li>));
     
+    const users = [
+      {
+        id: 102,
+        name: "João",
+        email: "joao@gmail.com",
+        avatar: "https:cdn.pixabay.com/photo/2016/08/20/05/38/avatar-1606916_640.png"
+      },
+      {
+        id: 77,
+        name: "Amélia",
+        email: "amelia@gmail.com",
+        avatar: "https:cdn.pixabay.com/photo/2017/01/31/13/05/cameo-2023867_640.png"
+      }
+    ];    
+    
     return (
       <div className="App">
       <Image source = 'https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg' alt='Cute cat staring' />
@@ -39,6 +55,7 @@ class App extends React.Component {
       <ul>
       { items }
       </ul>
+      {users.map((user, index) => <UserProfile user={ user } key={ index } />)}
       </div>
       );
     }
