589281ed5c8396024337e2779336e4bd44a12f86
heyynat <natalifplima@gmail.com>
2021-05-25 20:33:35 -0400

    Exercicios de fixaçao - Componentes React 2

--- front-end/bloco_11/dia_2/fixation-exercises-11-2/src/App.js
+++ front-end/bloco_11/dia_2/fixation-exercises-11-2/src/App.js
@@ -1,12 +1,47 @@
+import React from 'react';
 import './App.css';
 import Image from './Image';
+import Order from './Order';
 
-function App() {
-  return (
-    <div className="App">
+class App extends React.Component {
+  render() {
+    const headphone = {
+      id: 102,
+      user: "cena@gmail.com",
+      product: "Razer Headphone",
+      price: {
+        value: 99.99,
+        currency: "dollars"
+      }
+    };
+    
+    const energyDrink = {
+      id: 77,
+      user: "cena@gmail.com",
+      product: "Monster 500mL",
+      price: {
+        value: 9.99,
+        currency: "dollars"
+      }
+    };
+    
+    const shoppingList = ['leite', 'arroz', 'feijão', 'banana', 'carne'];
+    const items = shoppingList.map((item, index) => (<li key={ index }>{ item }</li>));
+    
+    return (
+      <div className="App">
       <Image source = 'https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg' alt='Cute cat staring' />
-    </div>
-  );
-}
-
-export default App;
+      
+      <h1> Orders recently created </h1>
+      <Order order={energyDrink} />
+      <Order order={headphone} />
+      <h2>Lista de compras</h2>
+      <ul>
+      { items }
+      </ul>
+      </div>
+      );
+    }
+  }
+  
+  export default App;
