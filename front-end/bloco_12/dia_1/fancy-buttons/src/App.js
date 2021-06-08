36843e7fb1c728c396dc9d8758df8c59da9874f6
heyynat <natalifplima@gmail.com>
2021-06-08 11:12:46 -0400

    Exercicio de fixaçao 3 e 4 -  dia 2

--- front-end/bloco_12/dia_1/fancy-buttons/src/App.js
+++ front-end/bloco_12/dia_1/fancy-buttons/src/App.js
@@ -1,26 +1,34 @@
 import React from 'react';
 import './App.css';
 
-function handleClick() {
-  console.log('Clicou no botão 1!')
-}
-
-function handleClick2() {
-  console.log('Clicou no botão 2!')
-}
-
-function handleClick3() {
-  console.log('Clicou no botão 3!')
-}
-
 class App extends React.Component {
+  constructor() {
+    super()
+    // A função abaixo vincula "manualmente" o `this` à nossa função
+    this.handleClick = this.handleClick.bind(this)
+    this.handleClick2 = this.handleClick2.bind(this)
+    this.handleClick3 = this.handleClick3.bind(this)
+
+  }
+
+  handleClick() {
+    console.log('Clicou no botão 1!')
+  }
+  
+  handleClick2() {
+    console.log('Clicou no botão 2!')
+  }
+  
+  handleClick3() {
+    console.log('Clicou no botão 3!')
+  }
 
   render() {
     return (
     <>
-    <button onClick={handleClick}>Meu botão 1</button>
-    <button onClick={handleClick2}>Meu botão 2 </button>
-    <button onClick={handleClick3}>Meu botão 3</button>
+    <button onClick={this.handleClick}>Meu botão 1</button>
+    <button onClick={this.handleClick2}>Meu botão 2 </button>
+    <button onClick={this.handleClick3}>Meu botão 3</button>
     </>
     )
   }
