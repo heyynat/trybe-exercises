bd6459c280ba5e2291486ee93a0309cded8dedfe
heyynat <natalifplima@gmail.com>
2021-06-08 11:07:17 -0400

    Exercicio de fixaçao 2 -  dia 2

--- front-end/bloco_12/dia_1/fancy-buttons/src/App.js
+++ front-end/bloco_12/dia_1/fancy-buttons/src/App.js
@@ -1,20 +1,28 @@
 import React from 'react';
 import './App.css';
 
-/* Embora isso funcione, essa DEFINITIVAMENTE
-não é a maneira correta de se criar eventos
-em React! A função se refere ao componente,
-então deve ser parte de sua classe! */
 function handleClick() {
-  console.log('Clicou no botão!')
+  console.log('Clicou no botão 1!')
+}
+
+function handleClick2() {
+  console.log('Clicou no botão 2!')
+}
+
+function handleClick3() {
+  console.log('Clicou no botão 3!')
 }
 
 class App extends React.Component {
-  /* Repare que, diferentemente do HTML, no
-  JSX você associa uma função a um evento
-  passando a própria função entre chaves `{}` */
+
   render() {
-    return <button onClick={handleClick}>Meu botão</button>
+    return (
+    <>
+    <button onClick={handleClick}>Meu botão 1</button>
+    <button onClick={handleClick2}>Meu botão 2 </button>
+    <button onClick={handleClick3}>Meu botão 3</button>
+    </>
+    )
   }
 }
 
