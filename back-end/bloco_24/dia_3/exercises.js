51be9309b565b0360672b9f3cb7a7ba81f4f03f1
heyynat <natalifplima@gmail.com>
2021-09-15 17:36:33 -0400

    Exercicio 2 -  dia 3

--- back-end/bloco_24/dia_3/exercises.js
+++ back-end/bloco_24/dia_3/exercises.js
@@ -1,2 +1,4 @@
 // Utilizando o operador $all , retorne todos os filmes que contenham action e adventure no array category .
 db.movies.find({ category: { $all: ["action", "adventure"] } }).pretty();
+
+// Agora retorne os filmes que contenham action no array category e possuem nota do IMDB maior do que 7 .
