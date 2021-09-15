cce0279dd83b90e29912efb630b3a59ddc8a7279
heyynat <natalifplima@gmail.com>
2021-09-15 18:07:30 -0400

    Exercicios 6 (correto) - dia 3

--- back-end/bloco_24/dia_3/exercises.js
+++ back-end/bloco_24/dia_3/exercises.js
@@ -29,7 +29,7 @@ db.movies.updateMany(
 
 // Retorne todos os filmes com ratings maior do que 103 , exibindo apenas os campos title e ratings .
 db.movies.find(
-    { ratings: { $elemMatch: { $gte: 100, $lt: 105 } } },
+    { ratings: { $elemMatch: { $gt: 103 } } },
     { title: 1, ratings: 1, _id: 0 }
 ).pretty();
 
