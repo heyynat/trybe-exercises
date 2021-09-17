ff1ac06e9709c66a0cd8cff07622852463902eff
heyynat <natalifplima@gmail.com>
2021-09-17 17:51:43 -0400

    Exercicio 4 - dia 1

--- back-end/bloco_25/dia_1/exercises_day1_block25.js
+++ back-end/bloco_25/dia_1/exercises_day1_block25.js
@@ -33,3 +33,16 @@ db.cliente.aggregate(
     }
   ]
 );
+
+// Exercício 4: Conte quantos clientes do estado SC existem na coleção. Retorne um documento em que o campo _id contenha a UF e outro campo com o total.
+db.clientes.aggregate([
+  {
+    $match: { uf: "SC" }
+  },
+  {
+    $group: {
+      _id: "$uf",
+      total: { $sum: 1 }
+    }
+  }
+]);
