861abcff717dc9902cc488cfb4e109d3cbdf0eb9
heyynat <natalifplima@gmail.com>
2021-09-17 17:54:28 -0400

    Exercicio 5 - dia 1

--- back-end/bloco_25/dia_1/exercises_day1_block25.js
+++ back-end/bloco_25/dia_1/exercises_day1_block25.js
@@ -46,3 +46,13 @@ db.clientes.aggregate([
     }
   }
 ]);
+
+// Exercício 5: Agrupe os clientes por sexo . Retorne o total de clientes de cada sexo no campo total .
+db.clientes.aggregate([
+  {
+    $group: {
+      _id: "$sexo",
+      total: { $sum: 1 }
+    }
+  }
+]);
