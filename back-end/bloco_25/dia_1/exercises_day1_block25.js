83ce9644217bf836168ff5d6eb9c7d231a29ef21
heyynat <natalifplima@gmail.com>
2021-09-17 17:47:45 -0400

    Exercicio 3 - dia 1

--- back-end/bloco_25/dia_1/exercises_day1_block25.js
+++ back-end/bloco_25/dia_1/exercises_day1_block25.js
@@ -16,3 +16,20 @@ db.cliente.aggregate(
     }
   ]
 );
+
+// Exercício 3: Utilizando o estágio $match , escreva uma agregação para retornar somente os clientes do sexo "FEMININO" e com data de nascimento entre os anos de 1995 e 2005 , limitando a quantidade de documentos retornados em 5 .
+db.cliente.aggregate(
+  [
+    {
+      $match: {
+        $and: [
+          { sexo : "FEMININO" } ,
+          { dataNascimento: { $in: ["1995-mm-ddTHH:MM:ssZ", "2005-mm-ddTHH:MM:ssZ"] } }
+        ]
+      }
+    },
+    {
+      $limit : 5
+    }
+  ]
+);
