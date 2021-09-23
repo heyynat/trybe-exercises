e6511b63c66b5ef202642c795239fd3989eeb98b
heyynat <natalifplima@gmail.com>
2021-09-23 18:34:29 -0400

    Exercicio 3

--- back-end/bloco_26/dia_1/my-scripts/imc.js
+++ back-end/bloco_26/dia_1/my-scripts/imc.js
@@ -1,3 +1,8 @@
+const readline = require('readline-sync');
+
+const peso = readline.questionInt('Qual seu peso? ');
+const altura = readline.questionInt('Qual sua altura? ');
+
 const infoImc = (peso, altura) => (peso / Math.pow(altura / 100, 2)).toFixed(2);
 
-console.log(infoImc(67, 169));
+console.log(infoImc(peso, altura));
