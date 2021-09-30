21e5a11dad18ae331d9c19970ba5a37d42a49299
heyynat <natalifplima@gmail.com>
2021-09-29 20:02:14 -0400

    Exercicio 2 e 3

--- back-end/bloco_26/dia_4/index.js
+++ back-end/bloco_26/dia_4/index.js
@@ -1,8 +1,25 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
 
 app.get('/ping', function (req, res) {
     res.status(200).json({ message: 'pong'});
+});
+
+app.post('/hello', (req, res) => {
+  const { name } = req.body;
+  res.status(200).json({ "message": `Hello, ${name}!` })
+})
+
+app.post('/greetings', (req, res) => {
+  const { nome, idade } = req.body;
+
+  if (parseInt(idade, 10) <= 17) {
+    return res.status(401).json({ message: `Unauthorized` });
+  }
+
+  res.status(200).json({ "name": `${nome}`, "age": `${idade}` });
 });
 
 app.listen(3001, () => {
