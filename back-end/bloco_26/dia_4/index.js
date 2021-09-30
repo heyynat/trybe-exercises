98e8fe28e698979d0dfc1e94ea3814def07dc0ff
heyynat <natalifplima@gmail.com>
2021-09-29 21:20:42 -0400

    Exercicios 6 e 7

--- back-end/bloco_26/dia_4/index.js
+++ back-end/bloco_26/dia_4/index.js
@@ -34,7 +34,22 @@ app.put('/usuario/:nome/:idade', function (req, res) {
 app.get('/simpsons', rescue(async (req, res) => {
   const simpsons = await simpsonsUtils.getSimpsons();
   res.status(200).json(simpsons);
-}))
+}));
+
+app.get(
+  '/simpsons/:id',
+  rescue(async (req, res) => {
+    const simpsons = await simpsonsUtils.getSimpsons();
+
+    const simpson = simpsons.find(({ id }) => id === req.params.id);
+
+    if (!simpson) {
+      return res.status(404).json({ message: 'simpson not found' });
+    }
+
+    return res.status(202).json(simpson);
+  })
+);
 
 app.listen(3001, () => {
   console.log('Aplicação ouvindo na porta 3001');
